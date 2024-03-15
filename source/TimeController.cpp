6af76b9aa2f134388b12739f5bae844f748cd611
LazyUnigineDude <lazy.unigine@gmail.com>
2024-03-15 01:35:31 -0400

    Add

--- source/TimeController.cpp
+++ source/TimeController.cpp
@@ -1,1 +1,72 @@
 #include "TimeController.h"
+REGISTER_COMPONENT(TimeController)
+
+
+void TimeController::Init() {
+
+	Unigine::GuiPtr GUI = Unigine::Gui::getCurrent();
+
+	AsyncLabel = Unigine::WidgetLabel::create();
+	AsyncLabel->setFontSize(16);
+	AsyncLabel->setPosition(50, 100);
+
+	UpdateLabel = Unigine::WidgetLabel::create();
+	UpdateLabel->setFontSize(16);
+	UpdateLabel->setPosition(50, 125);
+
+	PhysicsLabel = Unigine::WidgetLabel::create();
+	PhysicsLabel->setFontSize(16);
+	PhysicsLabel->setPosition(50, 150);
+
+	GUI->addChild(AsyncLabel, GUI->ALIGN_EXPAND | GUI->ALIGN_OVERLAP);
+	GUI->addChild(UpdateLabel, GUI->ALIGN_EXPAND | GUI->ALIGN_OVERLAP);
+	GUI->addChild(PhysicsLabel, GUI->ALIGN_EXPAND | GUI->ALIGN_OVERLAP);
+
+
+	TimeSlider = Unigine::WidgetSlider::create(1, 5, 1);
+	TimeSlider->setWidth(150);
+	TimeSlider->setPosition(100, 200);
+	TimeSlider->getEventChanged().connect([&]() {Time = 0; });
+	GUI->addChild(TimeSlider, GUI->ALIGN_EXPAND | GUI->ALIGN_OVERLAP);
+}
+
+void TimeController::UpdateAsync() {
+
+	std::string _Speed = "Async Time: " + FloatToString(Unigine::Game::getTime(), 2) + " Seconds";
+	AsyncLabel->setText(_Speed.c_str());
+}
+
+void TimeController::Update() {
+
+	std::string _Speed = "Update: " + FloatToString(Unigine::Game::getIFps()) + " Seconds";
+	UpdateLabel->setText(_Speed.c_str());
+	MoveObject();
+}
+
+void TimeController::UpdatePhysics() {
+
+	std::string _Speed = "Physics: " + FloatToString(Unigine::Physics::getIFps()) + " Seconds";
+	PhysicsLabel->setText(_Speed.c_str());
+}
+
+void TimeController::Shutdown() {
+	
+	Unigine::GuiPtr GUI = Unigine::Gui::getCurrent();
+
+	if (GUI->isChild(AsyncLabel)) GUI->removeChild(AsyncLabel);
+	if (GUI->isChild(UpdateLabel)) GUI->removeChild(UpdateLabel);
+	if (GUI->isChild(PhysicsLabel)) GUI->removeChild(PhysicsLabel);
+
+
+	AsyncLabel.deleteLater();
+	UpdateLabel.deleteLater();
+	PhysicsLabel.deleteLater();
+}
+
+void TimeController::MoveObject() {
+
+	Time += Unigine::Game::getIFps();
+	Unigine::Math::Vec3 Pos = Unigine::Math::lerp(Unigine::Math::Vec3_zero, Unigine::Math::Vec3_left * -4, Time/TimeSlider->getValue());
+	node->setWorldPosition(Pos);
+	if (Time > TimeSlider->getValue()) Time = 0;
+}
--- source/TimeController.h
+++ source/TimeController.h
@@ -1,5 +1,34 @@
 #pragma once
-class TimeController
-{
+#include <UnigineComponentSystem.h>
+#include <UnigineGame.h>
+#include <UnigineAsyncQueue.h>
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+class TimeController : public Unigine::ComponentBase {
+
+public:
+	COMPONENT_DEFINE(TimeController, ComponentBase)
+		COMPONENT_INIT(Init)
+		COMPONENT_UPDATE(Update)
+		COMPONENT_UPDATE_PHYSICS(UpdatePhysics)
+		COMPONENT_UPDATE_ASYNC_THREAD(UpdateAsync)
+		COMPONENT_SHUTDOWN(Shutdown)
+
+private:
+	void Init(), UpdateAsync(), Update(), UpdatePhysics(), Shutdown();
+	void MoveObject();
+
+	Unigine::WidgetLabelPtr AsyncLabel, UpdateLabel, PhysicsLabel;
+	Unigine::WidgetSliderPtr TimeSlider;
+	float Time;
 };
+
+std::string FloatToString(float value, int percision = 8) {
+	std::ostringstream stream;
+	stream << std::fixed << std::setprecision(percision) << value;
+	return stream.str();
+}
 
