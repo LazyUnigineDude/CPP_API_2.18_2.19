25376f8dfe828a0e0123b9ece2c3232bd093c5db
LazyUnigineDude <lazy.unigine@gmail.com>
2024-08-07 13:18:37 -0400

    Add

--- source/GUI/Widgets.cpp
+++ source/GUI/Widgets.cpp
@@ -3,9 +3,18 @@ REGISTER_COMPONENT(Widgets);
 
 void Widgets::Init() {
 
+	Button = Unigine::WidgetButton::create("Button 1");
+	Button->setWidth(50);
+	Button->setPosition(50, 50);
 
+	Unigine::GuiPtr GUI = Unigine::Gui::getCurrent();
+	GUI->addChild(Button, GUI->ALIGN_EXPAND | GUI->ALIGN_OVERLAP);
 }
 
 void Widgets::Shutdown() {
 
+	Unigine::GuiPtr GUI = Unigine::Gui::getCurrent();
+	if (GUI->isChild(Button)) GUI->removeChild(Button);
+
+	if (Button) Button.deleteLater();
 }
