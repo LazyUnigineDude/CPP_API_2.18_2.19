563d45f31ec55857241c0a275af15015b8ba15f4
LazyUnigineDude <lazy.unigine@gmail.com>
2024-09-18 22:13:15 -0400

    Add

---
+++ source/Character/IKSolver.cpp
@@ -0,0 +1,14 @@
+#include "IKSolver.h"
+REGISTER_COMPONENT(IKSolver);
+
+void IKSolver::Init()
+{
+}
+
+void IKSolver::Update()
+{
+}
+
+void IKSolver::Shutdown()
+{
+}
--- source/Character/IKSolver.h
+++ source/Character/IKSolver.h
@@ -5,5 +5,14 @@
 
 struct IKSolver : public Unigine::ComponentBase {
 
+public:
+	COMPONENT_DEFINE(IKSolver, ComponentBase);
+	COMPONENT_INIT(Init);
+	COMPONENT_UPDATE(Update);
+	COMPONENT_SHUTDOWN(Shutdown);
 
+protected:
+	PROP_ARRAY(Int, LLeg);
+	PROP_ARRAY(Int, RLeg);
+	void Init(), Update(), Shutdown();
 };
