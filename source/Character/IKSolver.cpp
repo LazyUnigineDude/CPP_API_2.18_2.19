77821609f6322c71fb199e0b85c9656d5b364a42
LazyUnigineDude <lazy.unigine@gmail.com>
2024-09-19 18:11:27 -0400

    Add

--- source/Character/IKSolver.cpp
+++ source/Character/IKSolver.cpp
@@ -1,14 +1,51 @@
 #include "IKSolver.h"
 REGISTER_COMPONENT(IKSolver);
 
-void IKSolver::Init()
-{
+void IKSolver::Init() {
+
+	Ptr = Unigine::WorldIntersectionNormal::create();
+
+	Animation = Unigine::static_ptr_cast<Unigine::ObjectMeshSkinned>(node);
+
+	IKLeft = Animation->addIKChain();
+	for (int i = 0; i < LLeg.size(); i++) 
+		Animation->addIKChainBone(LLeg[i], IKLeft);
+
+
+	IKRight = Animation->addIKChain();
+	for (int i = 0; i < RLeg.size(); i++)
+		Animation->addIKChainBone(LLeg[i], IKRight);
+
+	Animation->addVisualizeIKChain(IKLeft);
+	Animation->addVisualizeIKChain(IKRight);
 }
 
-void IKSolver::Update()
-{
+void IKSolver::Update() {
+
+	Animation->setIKChainPoleWorldPosition(node->getChild(0)->getWorldPosition(), IKLeft);
+	Animation->setIKChainPoleWorldPosition(node->getChild(1)->getWorldPosition(), IKRight);
+
+	AlignFoot(Animation->getBoneWorldTransform(LLeg[0]), IKLeft);
+	AlignFoot(Animation->getBoneWorldTransform(RLeg[0]), IKRight);
+
 }
 
-void IKSolver::Shutdown()
-{
+void IKSolver::Shutdown() {
+
+	if (Ptr) Ptr.deleteLater();
+}
+
+void IKSolver::AlignFoot(const Unigine::Math::Mat4& BoneTransform, const int& BoneNum) {
+
+	Unigine::ObjectPtr Obj = Unigine::World::getIntersection(BoneTransform.getTranslate(), BoneTransform.getTranslate() + Unigine::Math::Vec3_down, GroundMask, Ptr);
+	if (!Obj) return;
+
+	Unigine::Math::Vec3 Pos{ Ptr->getPoint() };
+	Unigine::Math::quat Rot{ BoneTransform.getRotate() };
+
+	Unigine::Math::vec3 Nor{ Ptr->getNormal()};
+	Rot = Unigine::Math::rotationFromTo(Unigine::Math::vec3_up, Nor) * Rot;
+
+	Animation->setIKChainTargetWorldPosition(Pos + (Unigine::Math::Vec3_up * AnkleHeight), BoneNum);
+	Animation->setIKChainEffectorWorldRotation(Rot, BoneNum);
 }
--- source/Character/IKSolver.h
+++ source/Character/IKSolver.h
@@ -12,7 +12,15 @@ public:
 	COMPONENT_SHUTDOWN(Shutdown);
 
 protected:
+	PROP_PARAM(Mask, GroundMask);
+	PROP_PARAM(Float, AnkleHeight, 1.0f);
 	PROP_ARRAY(Int, LLeg);
 	PROP_ARRAY(Int, RLeg);
 	void Init(), Update(), Shutdown();
+
+private:
+	int IKLeft, IKRight;
+	Unigine::ObjectMeshSkinnedPtr Animation;
+	void AlignFoot(const Unigine::Math::Mat4& BoneTransform, const int& BoneNum);
+	Unigine::WorldIntersectionNormalPtr Ptr;
 };
